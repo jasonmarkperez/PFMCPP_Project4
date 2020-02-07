923806e573c113bcf92eea7dbb4d9f08f083c621
Jason Perez <jaeysin@gmail.com>
2020-02-07 03:21:26 +0000

    Fix fixmes to account for divide by zero

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 /*
  Project 4 - Part 1 / 9
  video: Chapter 2 Part 7
@@ -33,107 +34,138 @@
  Wait for my code review.
  */
 
+struct FloatType
+{
+    float add(float lhs, float rhs)
+    {
+        return lhs + rhs;
+    }
+
+    float subtract(float lhs, float rhs)
+    {
+        return lhs - rhs;
+    }
+
+    float multiply(float lhs,float rhs )
+    {
+        return lhs * rhs;
+    }
+
+    float divide(float lhs,float rhs )
+    {
+        if(rhs != 0.f)
+        {
+            return lhs / rhs;
+        } else 
+        {
+            std::cout << "cannot divide by 0" << std::endl;
+            return 0;
+        }
+    }
+};
+
+struct DoubleType
+{
+    double add(double lhs, double rhs)
+    {
+        return lhs + rhs;
+    }
+
+    double subtract(double lhs, double rhs)
+    {
+        return lhs - rhs;
+    }
+
+    double multiply(double lhs, double rhs)
+    {
+        return lhs * rhs;
+    }
+
+    double divide(double lhs, double rhs)
+    {
+        if(rhs != 0.0)
+        {
+            return lhs / rhs;
+        } else 
+        {
+            std::cout << "cannot divide by 0.0" << std::endl;
+            return 0;
+        }
+    }
+};
+
+struct IntType
+{
+    int add(int lhs, int rhs)
+    {
+        return lhs + rhs;
+    }
+
+    int subtract(int lhs, int rhs)
+    {
+        return lhs - rhs;
+    }
+
+    int multiply(int lhs, int rhs)
+    {
+        return lhs * rhs;
+    }
+
+    int divide(int lhs, int rhs)
+    {
+        if(rhs != 0)
+        {
+            return lhs / rhs;
+        } else 
+        {
+            std::cout << "cannot divide by 0" << std::endl;
+            return 0;
+        }
+    }
+};
+
 #include <iostream>
 int main()
 {
-    struct FloatType FIXME do not define types inside main(). 
-    {
-        float add(float lhs, float rhs)
-        {
-            return lhs + rhs;
-        }
 
-        float subtract(float lhs, float rhs)
-        {
-            return lhs - rhs;
-        }
-
-        float multiply(float lhs,float rhs )
-        {
-            return lhs * rhs;
-        }
-
-        float divide(float lhs,float rhs )
-        {
-            return lhs / rhs; FIXME warn user if rhs == 0.f
-        }
-    };
-
-    struct DoubleType
-    {
-        double add(double lhs, double rhs)
-        {
-            return lhs + rhs;
-        }
-
-        double subtract(double lhs, double rhs)
-        {
-            return lhs - rhs;
-        }
-
-        double multiply(double lhs, double rhs)
-        {
-            return lhs * rhs;
-        }
-
-        double divide(double lhs, double rhs)
-        {
-            return lhs / rhs;   FIXME warn user if rhs == 0.0
-        }
-    };
-
-    struct IntType
-    {
-        int add(int lhs, int rhs)
-        {
-            return lhs + rhs;
-        }
-
-        int subtract(int lhs, int rhs)
-        {
-            return lhs - rhs;
-        }
-
-        int multiply(int lhs, int rhs)
-        {
-            return lhs * rhs;
-        }
-
-        int divide(int lhs, int rhs)
-        {
-            return lhs / rhs; FIXME handle case where rhs == 0.  integer division by 0 will crash the program.
-        }
-    };
 
     FloatType ft;
     auto floatAddResult = ft.add(23.90f, 99.1245f);
     auto floatSubtractResult = ft.subtract(924, 12.42f);
     auto floatMultiplyResult = ft.multiply(12.415f, 2.0f);
     auto floatDivideResult = ft.divide(2.20f, 21.4f);
+    auto floatDivideByZero = ft.divide(2.20f, 0.0f);
+
     std::cout << "float add result: " << floatAddResult << std::endl;
     std::cout << "float subtract result: " << floatSubtractResult << std::endl;
     std::cout << "float multiple result: " << floatMultiplyResult << std::endl;
     std::cout << "float divide result: " << floatDivideResult << std::endl;
+    std::cout << "float divide by zero result: " << floatDivideByZero << std::endl;
 
     DoubleType dt;
     auto doubleAddResult = dt.add(2333.90, 969.1245);
     auto doubleSubtractResult = dt.subtract(924, 12.42);
     auto doubleMultiplyResult = dt.multiply(122.415, 2.0);
     auto doubleDivideResult = dt.divide(24.20, 21.4);
+    auto doubleDivideByZero = dt.divide(24.20, 0);
+
     std::cout << "double add result: " << doubleAddResult << std::endl;
     std::cout << "double subtract result: " << doubleSubtractResult << std::endl;
     std::cout << "double multiple result: " << doubleMultiplyResult << std::endl;
     std::cout << "double divide result: " << doubleDivideResult << std::endl;
+    std::cout << "double divide by zero result: " << doubleDivideByZero << std::endl;
+
 
     IntType it;
     auto intAddResult = it.add(2333, 969);
     auto intSubtractResult = it.subtract(924, 12);
     auto intMultiplyResult = it.multiply(122, 2);
     auto intDivideResult = it.divide(24, 2);
+    auto intDivideByZero = it.divide(24, 0);
+
     std::cout << "int add result: " << intAddResult << std::endl;
     std::cout << "int subtract result: " << intSubtractResult << std::endl;
     std::cout << "int multiple result: " << intMultiplyResult << std::endl;
     std::cout << "int divide result: " << intDivideResult << std::endl;
-
-    std::cout << "good to go!" << std::endl;
+    std::cout << "int divide by zero: " << intDivideByZero << std::endl;
 }
